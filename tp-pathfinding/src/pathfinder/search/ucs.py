43fbe877058fd610878581a7bb09e3afb8f4d4ee
unknown <fede.z200889@gmail.com>
2025-03-27 23:04:20 -0300

    CREANDO EL ALGORITMO UCS

--- tp-pathfinding/src/pathfinder/search/ucs.py
+++ tp-pathfinding/src/pathfinder/search/ucs.py
@@ -1,5 +1,5 @@
 from ..models.grid import Grid
-from ..models.frontier import StackFrontier
+from ..models.frontier import PriorityQueueFrontier
 from ..models.solution import NoSolution, Solution
 from ..models.node import Node
 
@@ -24,4 +24,49 @@ class UniformCostSearch:
         # Add the node to the explored dictionary
         explored[node.state] = True
         
-        return NoSolution(explored)
+         # Return if the node contains a goal state
+        if node.state == grid.end:
+            return Solution(node, explored)
+
+        # Initialize the frontier with the initial node
+        # In this example, the frontier is a queue
+        frontier = PriorityQueueFrontier()
+        frontier.add(node)
+
+        while True:
+
+            #  Fail if the frontier is empty
+            if frontier.is_empty():
+                return NoSolution(explored)
+
+            # Remove a node from the frontier
+            node = frontier.pop()
+
+            # UCS
+            successors = grid.get_neighbours(node.state)
+
+            # Itera sobre el diccionario succesors y desempaqueta cada par clave/valor
+            for key, value in successors.items():
+
+                # Get the successor
+                new_state = value
+
+                # Check if the successor is not reached
+                if new_state not in explored:
+
+                    # Initialize the son node
+                    new_node = Node("", new_state,
+                                    node.cost + grid.get_cost(new_state),
+                                    parent=node, action=value)
+
+                    # Mark the successor as reached
+                    explored[new_state] = True
+
+                    # Return if the node contains a goal state
+                    # In this example, the goal test is run
+                    # before adding a new node to the frontier
+                    if new_state == grid.end:
+                        return Solution(new_node, explored)
+
+                    # Add the new node to the frontier
+                    frontier.add(new_node)
